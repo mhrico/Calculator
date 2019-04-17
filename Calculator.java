368c7bc99cbc7f7357ea4ff7e8d84f19d808930e
Mehedi Hassan Rico <mhrico2@gmail.com>
2019-04-17 10:59:38 +0600

    Fixed some errors in the main Calculator class

--- Calculator.java
+++ Calculator.java
@@ -1,27 +1,29 @@
 public class Calculator {
     
     public static void main(String[] args) {
+
+        CalMultiplication multiplication = new CalMultiplication();
+        CalMultiplication addition = new CalAddtion();
+        CalMultiplication subtraction = new CalSubtraction();
+        CalMultiplication division = new CalDivsion();
+
         switch(args[0]) {
             case "add":
-                System.out.println(addition(args[1],args[2]));
+                System.out.println(addition.addition(args[1],args[2]));
                 break;
 
             case "subtract":
-                 System.out.println(subtraction(args[1],args[2]));
+                 System.out.println(subtraction.subtraction(args[1],args[2]));
                  break;
 
             case "multiply":
-                 System.out.println(multiplication(args[1],args[2]));
-                 break;
-
-             case "subtract":
-                 System.out.println(subtraction(args[1],args[2]));
+                 System.out.println(multiplication.multiplication(args[1],args[2]));
                  break;
 
             case "divide":
-                 System.out.println(subtraction(args[1],args[2]));
+                 System.out.println(division.division(args[1],args[2]));
                  break;
-                 
+
             default: System.out.println("Usage: add | subtract | multiply | divide First_number Secondnumber");
         }
     }
