67a310f9f0e54eab89f6cca9966705961fc2ecf0
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-04-17 10:00:45 +0600

    Creating a CalMultipication class and add multiplication action

---
+++ CalMultiplication.java
@@ -0,0 +1,10 @@
+public class CalMultiplication{
+	int result;
+	CalMultiplication(int a,int b){
+		result = a * b;
+	}
+
+	void printResult(){
+		System.out.pirntln("The result of multiplication is : " + result);
+	}
+}
