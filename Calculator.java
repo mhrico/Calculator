11db53d7adff6f964f4eb71c751725f7e460aae4
Mehedi Hassan Rico <mhrico2@gmail.com>
2019-04-17 10:53:18 +0600

    Added other method calls to Main Calculator class

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,28 @@
 public class Calculator {
     
     public static void main(String[] args) {
-        
+        switch(args[0]) {
+            case "add":
+                System.out.println(addition(args[1],args[2]));
+                break;
+
+            case "subtract":
+                 System.out.println(subtraction(args[1],args[2]));
+                 break;
+
+            case "multiply":
+                 System.out.println(multiplication(args[1],args[2]));
+                 break;
+
+             case "subtract":
+                 System.out.println(subtraction(args[1],args[2]));
+                 break;
+
+            case "divide":
+                 System.out.println(subtraction(args[1],args[2]));
+                 break;
+                 
+            default: System.out.println("Usage: add | subtract | multiply | divide First_number Secondnumber");
+        }
     }
 }
