b44d69f23ff935c169e9b56a0268e4d95fa90fdd
Khairul Bashar <khairulbashar24853@gmail.com>
2019-04-17 10:34:08 +0600

    Creating CalAddition java class

---
+++ CalAddition.java
@@ -0,0 +1,10 @@
+public class CalAddition{
+	int result;
+	CalAddition(int first_number , int second_number){
+		result = first_number + second_number;
+	}
+
+	int addition(){
+		return result;
+	}
+}
